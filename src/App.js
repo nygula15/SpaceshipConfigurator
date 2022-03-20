1d13a326d1855666c5d31ecacd343ae414017976
Latif Nygmet <nygula@gmail.com>
2022-03-20 23:11:06 +0200

    major changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 
+
 function App() {
 
   const [color1, setColor] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   const [warp1, setWarp] = useState(0)
   const [option1, setOption] = useState(0)
 
+  // const colorValue1 = [{id: 'color0', value: 0},{id: 'color3', value: 100},{id: 'color2', value: 100}];
   const colorValue = [0, 100, 100];
   const powerValue = [0, 200, 500];
   const warpValue = [0, 1000];
@@ -18,32 +20,32 @@ function App() {
   return (
     <div className="App">
       <div className="Desktop">
-      <Container className="Container">
-
-          <Col align="center">
-            <div>Spaceship configurator</div>
-          </Col>
-
+      <Container className="Container0">
+        <div  className="Container">
+        <Row align="center">
+            <div style={{"font-size": "25px}"}}>Spaceship configurator</div>
+        </Row>
         <Row>
+        
           <Col xs={8}>
             <Row>
               <div>Select color:</div>
               <Col xs={4}>
-                <div class="btn" onClick={() => setColor(colorValue[0])} className="buttonClick Short">
+                <div class="btn" keyonClick={() => setColor(colorValue[0])} className={color1 === colorValue[0] ? "buttonClickShort Clicked" : "buttonClickShort"}>
                   <div className="rectangle" style={{background: '#ffffff'}}> </div>
                   <div>+{colorValue[0]}€</div>
                   <div>Snow</div>
                 </div>
               </Col>
               <Col xs={4}>
-                <div class="btn" onClick={() => setColor(colorValue[1])} className="buttonClick Short" >
-                  <div className="rectangle" style={{background: "#FF7A00"}}> </div>
+                <div class="btn" type="radio" name="color" id="Volcano" onClick={() => setColor(colorValue[1])} className={color1 === colorValue[1] ? "buttonClickShort Clicked" : "buttonClickShort"}>
+                <div className="rectangle" style={{background: "#FF7A00"}}> </div>
                   <div>+{colorValue[1]}€</div>
                   <div>Volcano</div>
                 </div>
               </Col>
               <Col xs={4}>
-                <div class="btn" onClick={() => setColor(colorValue[2])} className="buttonClick Short">
+                <div class="btn" type="radio" name="color" id="Sky" onClick={() => setColor(colorValue[2])} className={color1 === colorValue[2] ? "buttonClickShort Clicked" : "buttonClickShort"}>
                   <div className="rectangle" style={{background: "#6BE4FF"}}> </div>
                   <div>+{colorValue[2]}€</div>
                   <div>Sky</div>
@@ -92,7 +94,13 @@ function App() {
                 <div class="btn" onClick={() => setOption(optionValue[0])} className="buttonClick">
                   <div>Basic</div>
                   <span>+{optionValue[0]}€</span>
-                  <div className="Bottom">sdfghj</div>
+                  <div className="Bottom">
+                    <ul>
+                      <li>Air conditioning</li>
+                      <li>Cloth seats</li>
+                      <li>Fm radio</li>
+                    </ul>
+                  </div>
                 </div>
                 
               </Col>
@@ -100,19 +108,35 @@ function App() {
                 <div class="btn" onClick={() => setOption(optionValue[1])} className="buttonClick">
                   <div>Sport</div>
                   <span>+{optionValue[1]}€</span>
+                  <div className="Bottom">
+                    <ul>
+                      <li>Air conditioning</li>
+                      <li>Cloth seats</li>
+                      <li>Fm radio</li>
+                      <li>Personal tech support</li>
+                    </ul>
+                  </div>
                 </div>
               </Col>
               <Col xs={4}>
                 <div class="btn" onClick={() => setOption(optionValue[2])} className="buttonClick">
                   <div>Lux</div>
                   <span>+{optionValue[2]}€</span>
+                    <ul>
+                      <li>Air conditioning</li>
+                      <li>Luxury seats</li>
+                      <li>Fm radio</li>
+                      <li>Chrome weels</li>
+                      <li>Window tint</li>
+                      <li>Subwoofer</li>
+                    </ul>
                 </div>
               </Col>
             </Row>
           </Col>
           <Col xs={4}>
             <br/>
-            <div className="buttonClick Right">
+            <div className="buttonClickShort Right">
               <Row>
                 <Col xs={8}><div>Base price:</div></Col>
                 <Col xs={4}><div>1000€</div></Col>
@@ -133,6 +157,7 @@ function App() {
             </div>
           </Col>
         </Row>
+        </div>
       </Container>
       </div>
     </div>
